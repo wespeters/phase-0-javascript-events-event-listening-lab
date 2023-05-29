404526bf18d61b841b938efd0f5f763996a3dbf4
Wesley Peters <wesleypeters@gmail.com>
2023-05-29 08:01:50 -0600

    adds funtion to activate event listener

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
+  const input = document.getElementById('button');
+  input.addEventListener('click',() =>
+  alert('I was clicked!'));
 }
-
-input.addEventListener('click', clickAlert)
-};
-
-
+addingEventListener()
